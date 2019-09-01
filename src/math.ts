f0640201a817b282651e735af22dab18857e59d9
Alex Mayfield <alexmax2742@gmail.com>
2019-09-01 19:15:30 -0400

    Fix e-mail address

--- src/math.ts
+++ src/math.ts
@@ -1,6 +1,6 @@
 /**
  * rock3d.js: A 3D rendering engine with a retro heart.
- * Copyright (c) 2018-2019 Alex Mayfield <alexmaq[0]742@gmail.com>
+ * Copyright (c) 2018-2019 Alex Mayfield <alexmax2742@gmail.com>
  * 
  * For conditions of distribution and use, see LICENSE file included with
  * source distribution.
