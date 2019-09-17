15c5811d97ab6f5ef13acaa4b33bea6ebaf79029
Alex Mayfield <alexmax2742@gmail.com>
2019-09-17 18:59:25 -0400

    Initial work on game client and server
    A simple websocket client and server.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
-module.exports = {
+const editor = {
+    name: 'editor',
     entry: './src/editor/index.ts',
     mode: "production",
     module: {
@@ -19,7 +20,7 @@ module.exports = {
         }]
     },
     output: {
-        filename: 'index.js',
+        filename: 'editor.js',
         path: path.resolve(__dirname, 'public/dist'),
         publicPath: '/dist/'
     },
@@ -27,3 +28,59 @@ module.exports = {
         extensions: ['.js', '.ts', '.jsx', '.tsx']
     }
 };
+
+const client = {
+    name: 'client',
+    entry: './src/client/index.ts',
+    mode: "production",
+    module: {
+        rules: [{
+            exclude: /node_modules/,
+            test: /\.tsx?$/,
+            use: 'ts-loader'
+        }, {
+            exclude: /node_modules/,
+            test: /\.png$/,
+            use: 'file-loader'
+        }, {
+            exclude: /node_modules/,
+            test: /\.(frag|vert)$/,
+            use: 'raw-loader'
+        }]
+    },
+    output: {
+        filename: 'client.js',
+        path: path.resolve(__dirname, 'public/dist'),
+        publicPath: '/dist/'
+    },
+    resolve: {
+        extensions: ['.js', '.ts', '.jsx', '.tsx']
+    }
+};
+
+const server = {
+    name: 'server',
+    entry: './src/server/index.ts',
+    mode: "production",
+    target: 'node',
+    module: {
+        rules: [{
+            exclude: /node_modules/,
+            test: /\.ts$/,
+            use: 'ts-loader'
+        }, {
+            exclude: /node_modules/,
+            test: /\.png$/,
+            use: 'file-loader'
+        }]
+    },
+    output: {
+        filename: 'server.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    resolve: {
+        extensions: ['.js', '.ts']
+    }
+};
+
+module.exports = [ editor, client, server ];
